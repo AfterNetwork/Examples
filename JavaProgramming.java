39018ae9f4fb195884525d8897744c031f1af25f
After <45772308+AfterNetwork@users.noreply.github.com>
2019-04-30 10:44:41 -0700

    Create JavaProgramming.java

---
+++ JavaProgramming.java
@@ -0,0 +1,6 @@
+class JavaProgramming  {
+  public static void main(String[] args) {
+    String hello = ("I can figure out how to make a string var and i am too lazy a .py file, i get lazy if i don't use it")
+    System.out.println(hello)  // End of line comment: I HATE THIS
+  }
+}
